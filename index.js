bfd96a25e75dc69d69fea316e4b502cee0a5e051
Kamogelo Mbele <kamogelokai88@gmail.com>
2025-04-15 11:44:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function toggleMenu() {
+    const navLinks = document.querySelector('.navbuttons');
+    navLinks.classList.toggle('active');
+}
+function toggleMode(){
+    document.body.classList.toggle("light-mode");
+}
